5ea87977d64e212ecafc806cd4a2f44e34b2f639
Ksenia Will <ksen.ka_v@icloud.com>
2024-04-07 22:15:24 +0300

    update_conftest

--- conftest.py
+++ conftest.py
@@ -3,14 +3,6 @@ from selenium import webdriver
 from data import Urls
 
 
-# @pytest.fixture(scope='function')
-# def driver():
-#     driver = webdriver.Firefox()
-#     driver = webdriver.Chrome()
-#     driver.get(Urls.main_page)
-#
-#     yield driver
-#     driver.quit()
 @pytest.fixture(params=['firefox', 'chrome'])
 def driver(request):
     if request.param == 'firefox':
